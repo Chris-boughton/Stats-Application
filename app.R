86b78743e35c06877cb33ac2bde76c02b1c73064
Breadkrums <boughtonc28@gmail.com>
2024-10-21 22:42:34 +0100

    Changes to app

--- app.R
+++ app.R
@@ -2,5 +2,5 @@
 # Author: Chris Boughton
 # Objective: To provide scientific statistical analysis through an easy to use application. Options should be provided to 
 # input the data, data types, variables and other metrics in order to correctly select the best statistical test to use. 
-# This should then produce a series of graphs, tables and worded paragraphs to jsutify the test chosen. Eventually a SLM could
-# be built underneath the application to produce summary text for scientific papers.
+# This should then produce a series of graphs, tables and worded paragraphs to justify the test chosen. Eventually a SLM could
+# be built underneath the application to produce summary text for scientific papers. here
